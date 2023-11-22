fb200318b1aedfc777bf5d87a6774717a1a4b8e5
Yash Raj <55595124+Yashraj2603@users.noreply.github.com>
2023-11-22 17:00:32 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,20 @@
+from flask import Flask , request , jsonify
+app = Flask(__name__)
+
+@app.route('/post_data', methods = ['POST'])
+def post_data:
+    try:
+        data = request.get_json(x)
+        print("number :", x)
+        if type(x) is int:
+            response = { "result": x*x}
+            return jsonify(response)
+        else:
+            error_response = {}
+            return jsonify (error_response),400      
+    except Exception as e:
+        error_response = {"result": 'error'}
+        return jsonify (error_response),405
+
+if __name__ == "__main__":
+    app.run(debug=True)
